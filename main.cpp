57f2538d570ebd78de02409be7d74f4f09c86b6a
Andreas Schäfer <gentryx@users.noreply.github.com>
2013-06-17 12:24:56 +0200

    basic working version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-// g++ -O2 -march=native -I /home/gentryx/libgeodecomp/src/ -L /home/gentryx/libgeodecomp/build/Linux-x86_64/ -lgeodecomp main.cpp -fopenmp -o sph_demo && echo go && time ./sph_demo
+// g++ -O2 -march=native -I /home/gentryx/libgeodecomp/src/ -L /home/gentryx/libgeodecomp/build/Linux-x86_64/ -lgeodecomp main.cpp -fopenmp -o nbody_demo && echo go && time ./nbody_demo
 #include <libgeodecomp.h>
 
 using namespace LibGeoDecomp;
+
+// fixme: tune
+// - loop order
+// - block size
+// - manual vectorization vs. automatic
+
 
 // void update() {
 //     // fixme: make precision configurable
@@ -38,14 +44,16 @@ using namespace LibGeoDecomp;
 //     // fixme: write back
 // }
 
-
 #define RECIPROCAL_SQUARE_ROOT(x) (1.0 / sqrt(x))
+#define FLOAT float
+
+typedef double FloatType;
+const int CONTAINER_SIZE = 512;
 
 class NBodyContainer
 {
 public:
-    static const int SIZE = 256;
-
+    static const int FORCE_OFFSET = 0.01;
     typedef Stencils::Moore<3, 1> Stencil;
     typedef Topologies::Cube<3>::Topology Topology;
     class API : public CellAPITraits::Fixed
@@ -59,9 +67,9 @@ public:
     inline NBodyContainer()
     {}
 
-    inline NBodyContainer(const Coord<3>& pos, const Coord<3>& dim)
+    inline NBodyContainer(const Coord<3>& pos)
     {
-        for (int i; i < SIZE; ++i) {
+        for (int i; i < CONTAINER_SIZE; ++i) {
             posX[i] = (i         % 10) * 0.1 + pos.x();
             posY[i] = ((i / 10)  % 10) * 0.1 + pos.y();
             posZ[i] = ((i / 100) % 10) * 0.1 + pos.z();
@@ -74,51 +82,81 @@ public:
     template<typename COORD_MAP>
     void update(const COORD_MAP& hood, const unsigned& nanoStep)
     {
-        const NBodyContainer& self = hood[FixedCoord<0, 0, 0>()];
+        const NBodyContainer& oldSelf = hood[FixedCoord<0, 0, 0>()];
 
-        for (int i = 0; i < SIZE; ++i) {
-            velX[i] = original.velX[i];
-            velY[i] = original.velY[i];
-            velZ[i] = original.velZ[i];
+        for (int i = 0; i < CONTAINER_SIZE; ++i) {
+            velX[i] = oldSelf.velX[i];
+            velY[i] = oldSelf.velY[i];
+            velZ[i] = oldSelf.velZ[i];
         }
 
 #define INTERACT(REL_X, REL_Y, REL_Z)                                   \
         {                                                               \
             const NBodyContainer& neighbor =                            \
                 hood[FixedCoord<REL_X, REL_Y, REL_Z>()];                \
-            for (int i = 0; i < SIZE; ++i) {                            \
-                for (int j = 0; j < SIZE; ++j) {                        \
-                    double deltaX = self.posX[i] - neighbor.posX[j];    \
-                    double deltaY = self.posY[i] - neighbor.posY[j];    \
-                    double deltaZ = self.posZ[i] - neighbor.posZ[j];    \
-                    double dist2 =                                      \
+            for (int i = 0; i < CONTAINER_SIZE; ++i) {                  \
+                for (int j = 0; j < CONTAINER_SIZE; ++j) {              \
+                    FLOAT deltaX = oldSelf.posX[i] - neighbor.posX[j];  \
+                    FLOAT deltaY = oldSelf.posY[i] - neighbor.posY[j];  \
+                    FLOAT deltaZ = oldSelf.posZ[i] - neighbor.posZ[j];  \
+                    FLOAT dist2 =                                       \
+                        FORCE_OFFSET +                                  \
                         deltaX * deltaX +                               \
                         deltaY * deltaY +                               \
                         deltaZ * deltaZ;                                \
-                    double force = RECIPROCAL_SQUARE_ROOT(dist2);       \
-                    velX[i] += forse * deltaX;                          \
-                    velY[i] += forse * deltaY;                          \
-                    velZ[i] += forse * deltaZ;                          \
+                    FLOAT force = RECIPROCAL_SQUARE_ROOT(dist2);        \
+                    velX[i] += force * deltaX;                          \
+                    velY[i] += force * deltaY;                          \
+                    velZ[i] += force * deltaZ;                          \
                 }                                                       \
             }                                                           \
         }
 
+        INTERACT(-1, -1, -1);
+        INTERACT( 0, -1, -1);
+        INTERACT( 1, -1, -1);
+        INTERACT(-1,  0, -1);
+        INTERACT( 0,  0, -1);
+        INTERACT( 1,  0, -1);
+        INTERACT(-1,  1, -1);
+        INTERACT( 0,  1, -1);
+        INTERACT( 1,  1, -1);
 
-        for (int i = 0; i < SIZE; ++i) {
-            posX[i] = original.posX[i] + velX[i];
-            posY[i] = original.posY[i] + velY[i];
-            posZ[i] = original.posZ[i] + velZ[i];
+        INTERACT(-1, -1,  0);
+        INTERACT( 0, -1,  0);
+        INTERACT( 1, -1,  0);
+        INTERACT(-1,  0,  0);
+        INTERACT( 0,  0,  0);
+        INTERACT( 1,  0,  0);
+        INTERACT(-1,  1,  0);
+        INTERACT( 0,  1,  0);
+        INTERACT( 1,  1,  0);
+
+        INTERACT(-1, -1,  1);
+        INTERACT( 0, -1,  1);
+        INTERACT( 1, -1,  1);
+        INTERACT(-1,  0,  1);
+        INTERACT( 0,  0,  1);
+        INTERACT( 1,  0,  1);
+        INTERACT(-1,  1,  1);
+        INTERACT( 0,  1,  1);
+        INTERACT( 1,  1,  1);
+
+        for (int i = 0; i < CONTAINER_SIZE; ++i) {
+            posX[i] = oldSelf.posX[i] + velX[i];
+            posY[i] = oldSelf.posY[i] + velY[i];
+            posZ[i] = oldSelf.posZ[i] + velZ[i];
         }
 
     }
 
 private:
-    double posX[SIZE];
-    double posY[SIZE];
-    double posZ[SIZE];
-    double velX[SIZE];
-    double velY[SIZE];
-    double velZ[SIZE];
+    FLOAT posX[CONTAINER_SIZE];
+    FLOAT posY[CONTAINER_SIZE];
+    FLOAT posZ[CONTAINER_SIZE];
+    FLOAT velX[CONTAINER_SIZE];
+    FLOAT velY[CONTAINER_SIZE];
+    FLOAT velZ[CONTAINER_SIZE];
 };
 
 class NBodyInitializer : public SimpleInitializer<NBodyContainer>
@@ -141,9 +179,18 @@ public:
 
 void runSimulation()
 {
-    int outputFrequency = 10;
-    int maxSteps = 100;
+    int outputFrequency = 100;
+    int maxSteps = 10 ;
     Coord<3> dim(3, 3, 3);
+
+    MPI::Aint displacements[] = {0};
+    MPI::Datatype memberTypes[] = {MPI::CHAR};
+    int lengths[] = {sizeof(NBodyContainer)};
+    MPI::Datatype objType;
+    objType =
+        MPI::Datatype::Create_struct(1, lengths, displacements, memberTypes);
+    objType.Commit();
+
 
     NBodyInitializer *init = new NBodyInitializer(dim, maxSteps);
 
@@ -152,14 +199,27 @@ void runSimulation()
         MPILayer().rank() ? 0 : new TracingBalancer(new NoOpBalancer()),
         maxSteps,
         1,
-        MPI::DOUBLE);
+        objType);
 
     if (MPILayer().rank() == 0) {
         sim.addWriter(
             new TracingWriter<NBodyContainer>(outputFrequency, init->maxSteps()));
     }
 
+    long long tStart = Chronometer::timeUSec();
     sim.run();
+    long long tEnd = Chronometer::timeUSec();
+
+    double seconds = 1e-6 * (tEnd - tStart);
+    double flops =
+        // time steps * grid size
+        1.0 * maxSteps * dim.prod() *
+        // interactions per container update
+        27 * CONTAINER_SIZE * CONTAINER_SIZE *
+        // FLOPs per interaction
+        (3 + 6 + 1 + 6);
+    double gflops = 1e-9 * flops / seconds;
+    std::cout << "GFLOPS: " << gflops << "\n";
 }
 
 
