946de0e0ffa69f03b442000025f15aed98a42c43
Andreas Schäfer <gentryx@users.noreply.github.com>
2013-07-03 21:23:12 +0200

    added first batch of output logs

---
+++ jobscript.sh
@@ -0,0 +1,14 @@
+# @ job_name = AndiTest
+# @ comment = "My Second Job"
+# @ error  = $(job_name).$(jobid).out
+# @ output = $(job_name).$(jobid).out
+# @ environment = COPY_ALL
+# @ wall_clock_limit = 00:10:00
+# @ notification = error
+# @ notify_user = gentryx@gmx.de
+# @ job_type = bluegene
+# @ bg_size = 1
+# @ queue
+echo "go go, powerrangers!"
+runjob --exe main --args "-t 1" --ranks-per-node 1
+echo "done"
